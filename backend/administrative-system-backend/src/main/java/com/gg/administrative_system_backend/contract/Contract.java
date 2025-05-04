86ad4f62201f32688d128531fcea05f873cef225
iMinato01 <gabrielmag559@gmail.com>
2025-05-03 21:09:17 -0600

    Refactorización del modelo Contract e implementación de lombok

--- backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/contract/Contract.java
+++ backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/contract/Contract.java
@@ -4,50 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Builder
+@Getter
+@Setter
 public class Contract {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
         private String name;
-        private boolean status;
-        private float totalExpenses;
-        public Contract(){}
-        public Contract(String name){
-            this.name = name;
-            this.status = true;
-            this.totalExpenses = 0;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public boolean isStatus() {
-            return status;
-        }
-
-        public void setStatus(boolean status) {
-            this.status = status;
-        }
-
-        public float getTotalExpenses() {
-            return totalExpenses;
-        }
-
-        public void setTotalExpenses(float totalExpenses) {
-            this.totalExpenses = totalExpenses;
-        }
+        @Builder.Default
+        private boolean status = true;
+        @Builder.Default
+        private float totalExpenses = 0;
 }
