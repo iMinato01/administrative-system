555b0883271571dc1c5085c459ce9e644ea7ba13
iMinato01 <gabrielmag559@gmail.com>
2025-06-14 23:44:00 -0600

    fix(utils): add default constructor and mark UpdateProperty with @Component

--- backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/util/UpdateProperty.java
+++ backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/util/UpdateProperty.java
@@ -1,9 +1,13 @@
 package com.gg.administrative_system_backend.util;
 
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-
+@Component
+@NoArgsConstructor
 public class UpdateProperty {
     public <T> void updateIfChanged(Supplier<T> getter, T newValue, Consumer<T> setter){
         if(newValue != null && !Objects.equals(getter.get(), newValue)){
