15cee86fbc7efa6c07f1dcff961303f8274c696b
Energy pc2 <gabrielmag559@gmail.com>
2025-06-27 14:40:49 -0600

    refactor(frontend): - add responsive in contract.html - Added reusable modal functions in functions.js

--- frontend/functions.js
+++ frontend/functions.js
@@ -1,8 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function hiddenElement(value) {
-        const element = document.getElementById(value);
-        element.classList.toggle('hidden');
-    }
 
-    document.getElementById('filter-button').addEventListener('click', () => hiddenElement("contract-filter"));
+  function toggleHidden(id) {
+    const el = document.getElementById(id);
+    el.classList.toggle('hidden');
+  }
+
+  function showElement(id) {
+    const el = document.getElementById(id);
+    el.classList.remove('hidden');
+  }
+
+  function hideElement(id) {
+    const el = document.getElementById(id);
+    el.classList.add('hidden');
+  }
+
+  // Filtros
+  document.getElementById('filter-button')?.addEventListener('click', () => toggleHidden("contract-filter"));
+  //Modales
+    document.getElementById('contract-open-add')?.addEventListener('click', () => showElement("contract-modal-add"));
+    document.getElementById('contract-close-modal-add')?.addEventListener('click', () => hideElement("contract-modal-add"));
+    document.getElementById('contract-open-edit')?.addEventListener('click', () => showElement("contract-modal-edit"));
+    document.getElementById('contract-close-modal-edit')?.addEventListener('click', () => hideElement("contract-modal-edit"));
+    document.getElementById("open-deactivate").addEventListener("click", () => showElement("contract-modal-deactivate"));
+    document.getElementById("contract-close-modal-deactivate").addEventListener("click", () => hideElement("contract-modal-deactivate"));
+    document.getElementById("open-activate").addEventListener("click", () => showElement("contract-modal-activate"));
+    document.getElementById("contract-close-modal-activate").addEventListener("click", () => hideElement("contract-modal-activate"));
 });
