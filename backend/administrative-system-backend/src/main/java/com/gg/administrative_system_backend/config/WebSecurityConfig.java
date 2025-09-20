843f0bfd660a437e27a2577e344a0da798d7287e
iMinato01 <gabrielmag559@gmail.com>
2025-09-19 23:20:32 -0600

    feat(security): configure authorizeHttpRequests for authentication and public endpoints

--- backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/config/WebSecurityConfig.java
+++ backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.gg.administrative_system_backend.config;
 
+import com.gg.administrative_system_backend.auth.CompanyDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,12 +11,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig{
+    private final CompanyDetailsService companyDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll());
+        http
                 .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/companies").permitAll()
+                        .anyRequest().authenticated());
         return http.build();
     }
 }
