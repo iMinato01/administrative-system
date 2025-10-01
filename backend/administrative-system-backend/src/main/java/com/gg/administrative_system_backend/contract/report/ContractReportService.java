0aa55bc937bed0eac1fa973341841711844720b0
Energy pc2 <gabrielmag559@gmail.com>
2025-10-01 10:07:05 -0600

    refactor(report): update ContractReportService to use static ReportUtils.getHeader - Removed dependency injection of ReportUtils - Replaced manual parameter mapping with centralized getHeader(company) - Simplified exportPdf method for consistency with new report header structure - Replaced SecurityContextHolder with AuthenticationUtils for cleaner authentication access

--- backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/contract/report/ContractReportService.java
+++ backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/contract/report/ContractReportService.java
@@ -5,38 +5,25 @@ import com.gg.administrative_system_backend.company.entity.Company;
 import com.gg.administrative_system_backend.company.service.CompanyService;
 import com.gg.administrative_system_backend.contract.service.ContractService;
 import com.gg.administrative_system_backend.shared.Report;
+import com.gg.administrative_system_backend.util.AuthenticatonUtils;
 import com.gg.administrative_system_backend.util.ReportHelper;
 import com.gg.administrative_system_backend.util.ReportUtils;
-import lombok.AllArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Service responsible for generating PDF reports for {@code Contract} entities.
  */
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ContractReportService {
     private final CompanyService companyService;
     private final ContractService contractService;
     private final ReportHelper reportHelper;
-    private final ReportUtils reportUtils;
 
-    /**
-     * Exports a PDF report containing all {@code Contract} entities,
-     * including the header data from the associated {@code Company}.
-     *
-     * @return A byte array representing the generated PDF document.
-     * @throws Exception if an error occurs during data retrieval or report generation.
-     */
-    public byte[] exportPdf() throws Exception{
-        Map<String, Object> parameters = new HashMap<>();
-        CompanyDetails companyDetails = (CompanyDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public byte[] exportPdf() throws Exception {
+        CompanyDetails companyDetails = (CompanyDetails) AuthenticatonUtils.getAuthentication().getPrincipal();
         Company company = companyService.findCompany(companyDetails.getId());
-        reportUtils.setReportHeader(parameters, company);
-        return reportHelper.generatePdf(Report.CONTRACT, parameters, contractService.findAll());
+        return reportHelper.generatePdf(Report.CONTRACT, ReportUtils.getHeader(company), contractService.findAll());
     }
 }
