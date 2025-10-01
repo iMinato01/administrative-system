d64882b2bb967e3177ce3c3d3415a2499cd94e01
Energy pc2 <gabrielmag559@gmail.com>
2025-10-01 09:47:27 -0600

    refactor(report): simplify ReportUtils to return header map directly

--- backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/util/ReportUtils.java
+++ backend/administrative-system-backend/src/main/java/com/gg/administrative_system_backend/util/ReportUtils.java
@@ -4,22 +4,28 @@ import com.gg.administrative_system_backend.company.entity.Company;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 @AllArgsConstructor
 public class ReportUtils {
-    private final ReportHelper reportHelper;
-    public void setReportHeader(Map<String, Object> parameters, Company company){
-        reportHelper.mapElements(company::getName, parameters, "NAME");
-        reportHelper.mapElements(company::getRfc, parameters, "RFC");
-        reportHelper.mapElements(company::getPhoneNumber, parameters, "PHONE_NUMBER");
-        reportHelper.mapElements(company::getStreet, parameters, "STREET");
-        reportHelper.mapElements(company::getPostalCode, parameters, "POSTAL_CODE");
-        reportHelper.mapElements(company::getExteriorNumber, parameters, "EXTERIOR_NUMBER");
-        reportHelper.mapElements(company::getInteriorNumber, parameters, "INTERIOR_NUMBER");
-        reportHelper.mapElements(company::getLocality, parameters, "LOCALITY");
-        reportHelper.mapElements(company::getMunicipality, parameters, "MUNICIPALITY");
-        reportHelper.mapElements(company::getState, parameters, "STATE");
+    public static Map<String, Object> getHeader(Company company){
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("ID", company.getId());
+        parameters.put("NAME", company.getName());
+        parameters.put("PASSWORD", company.getPassword());
+        parameters.put("ROLE", company.getRole().name());
+        parameters.put("STATUS", company.isStatus());
+        parameters.put("RFC", company.getRfc());
+        parameters.put("PHONE_NUMBER", company.getPhoneNumber());
+        parameters.put("STATE", company.getState());
+        parameters.put("MUNICIPALITY", company.getMunicipality());
+        parameters.put("LOCALITY", company.getLocality());
+        parameters.put("POSTAL_CODE", company.getPostalCode());
+        parameters.put("STREET", company.getStreet());
+        parameters.put("INTERIOR_NUMBER", company.getInteriorNumber());
+        parameters.put("EXTERIOR_NUMBER", company.getExteriorNumber());
+        return parameters;
     }
 }
